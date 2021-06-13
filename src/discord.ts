1bf87bb579ccdbff639c355745c0a6c0c243b815
Jayden Chan <jaydencn7@gmail.com>
2021-06-12 18:58:59 -0700

    JC: Late warning functionality

--- src/discord.ts
+++ src/discord.ts
@@ -3,8 +3,20 @@ import * as moment from "moment-timezone";
 import { ESEAItem } from "./types";
 import { log, error } from "./log";
 
+const ESEA_GREEN = "#0e9648";
+const RAT_IMAGE = "https://i.imgur.com/s6FV8zx.png";
+const TEAM_MEMBERS = [
+  "140677612178636800",
+  "195030314806935552",
+  "159144867723739136",
+  "194244725459386369",
+  "202996462953562121",
+];
+
 export async function initDiscord(): Promise<Discord.Client> {
-  const client = new Discord.Client();
+  let intents = new Discord.Intents(Discord.Intents.NON_PRIVILEGED);
+  intents.add(Discord.Intents.FLAGS.GUILD_MEMBERS);
+  const client = new Discord.Client({ ws: { intents } });
   const token = process.env.DISCORD_TOKEN;
   client.login(token);
 
@@ -21,12 +33,48 @@ export async function initDiscord(): Promise<Discord.Client> {
   });
 }
 
+export function allVCMembers(client: Discord.Client): string[] {
+  return [
+    ...client.guilds.cache
+      .map((g) => g.channels.cache.filter((c) => c.type === "voice"))
+      .flatMap((c) => [...c])
+      .reduce((acc, curr) => {
+        curr[1].members.forEach((m) => acc.add(m.user.id));
+        return acc;
+      }, new Set<string>()),
+  ];
+}
+
+export async function notifyWarmupLatecomers(
+  client: Discord.Client,
+  server: string,
+  channel: string,
+  lateUsers: string[]
+) {
+  const vcMembers = allVCMembers(client);
+  const missingMembers = TEAM_MEMBERS.filter(
+    (m) => !vcMembers.includes(m) && !lateUsers.includes(m)
+  );
+
+  if (missingMembers.length === 0) return;
+
+  const c = client.channels.cache.find(
+    (c) =>
+      c.isText() && "name" in c && c.name === channel && c.guild.name === server
+  )! as Discord.TextChannel;
+
+  const pings = missingMembers.map((id) => `<@${id}>`);
+
+  await c.send(`${pings.join(" ")} YOU ARE LATE FOR WARMUP!!!`);
+}
+
 export function getEmbed(item: ESEAItem): Discord.MessageEmbed {
+  const matchLink = `https://play.esea.net/match/${item.id}`;
   return new Discord.MessageEmbed()
-    .setColor("#0e9648")
+    .setColor(ESEA_GREEN)
     .setTitle("ESEA League Match Soon!")
-    .setURL(`https://play.esea.net/match/${item.id}`)
-    .setThumbnail("https://i.imgur.com/s6FV8zx.png")
+    .setURL(matchLink)
+    .setThumbnail(RAT_IMAGE)
     .addFields(
       { name: "Map", value: item.map.id },
       {
@@ -43,10 +91,10 @@ export function getEmbed(item: ESEAItem): Discord.MessageEmbed {
         name: "Away",
         value: `[${item.away.name}](https://play.esea.net/teams/${item.away.id})`,
       },
-      { name: "Homepage", value: `https://play.esea.net/match/${item.id}` }
+      { name: "Homepage", value: matchLink }
     )
     .setTimestamp()
-    .setFooter("ESEA Scheduling Bot", "https://i.imgur.com/s6FV8zx.png");
+    .setFooter("ESEA Scheduling Bot", RAT_IMAGE);
 }
 
 export async function sendMessage(
@@ -58,7 +106,7 @@ export async function sendMessage(
     server: string;
   }
 ) {
-  const c = client.channels.cache.find(
+  const channel = client.channels.cache.find(
     (c) =>
       c.isText() &&
       "name" in c &&
@@ -66,10 +114,10 @@ export async function sendMessage(
       c.guild.name === props.server
   )! as Discord.TextChannel;
 
-  const g = client.guilds.cache.find((g) => g.name === props.server)!;
-  const r = g.roles.cache.find((r) => r.name === props.role)!;
+  const guild = client.guilds.cache.find((g) => g.name === props.server)!;
+  const role = guild.roles.cache.find((r) => r.name === props.role)!;
 
-  await c.send(`<@&${r.id}> ${props.message}`);
+  await channel.send(`<@&${role.id}> ${props.message}`);
 }
 
 export async function sendEmbed(
@@ -81,7 +129,7 @@ export async function sendEmbed(
     server: string;
   }
 ) {
-  const c = client.channels.cache.find(
+  const channel = client.channels.cache.find(
     (c) =>
       c.isText() &&
       "name" in c &&
@@ -89,8 +137,8 @@ export async function sendEmbed(
       c.guild.name === props.server
   )! as Discord.TextChannel;
 
-  const g = client.guilds.cache.find((g) => g.name === props.server)!;
-  const r = g.roles.cache.find((r) => r.name === props.role)!;
+  const guild = client.guilds.cache.find((g) => g.name === props.server)!;
+  const role = guild.roles.cache.find((r) => r.name === props.role)!;
 
-  await c.send(`<@&${r.id}>`, props.message);
+  await channel.send(`<@&${role.id}>`, props.message);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,99 @@
 import * as moment from "moment-timezone";
 import * as Discord from "discord.js";
 
-import { initDiscord, getEmbed, sendEmbed, sendMessage } from "./discord";
+import {
+  initDiscord,
+  getEmbed,
+  sendEmbed,
+  sendMessage,
+  notifyWarmupLatecomers,
+} from "./discord";
+
 import { getMatches } from "./scrape";
 import { log, error } from "./log";
 import { ESEAData } from "./types";
 
 const SIX_HOURS = 6 * 60 * 60 * 1000;
+const FIFTEEN_MINS = 15 * 60 * 1000;
 const SERVER_NAME = "Rat With Gun eSports";
 const CHANNEL_NAME = "schedule";
 const ROLE_NAME = "RAT";
 
 let dayTimeout: NodeJS.Timeout | undefined;
 let warmupTimeout: NodeJS.Timeout | undefined;
+let lateTimeout: NodeJS.Timeout | undefined;
+let lateUsers: string[] = [];
+
+function checkMatchTomorrow(client: Discord.Client, data: ESEAData) {
+  const now = moment();
+  const hasMatchTomorrow = data.data.find((match) => {
+    const date = moment(match.date);
+    const diff = date.diff(now, "hours");
+
+    return diff < 31 && diff > 24;
+  });
+
+  if (hasMatchTomorrow) {
+    log(`Match for tomorrow found, scheduling notification`);
+
+    // Send the message 24 hours before the start date
+    const diffMs = moment(hasMatchTomorrow.date)
+      .subtract(24, "hours")
+      .diff(moment(), "ms");
+
+    if (diffMs > 0 && dayTimeout === undefined) {
+      dayTimeout = setTimeout(() => {
+        sendEmbed(client, {
+          message: getEmbed(hasMatchTomorrow),
+          server: SERVER_NAME,
+          channel: CHANNEL_NAME,
+          role: ROLE_NAME,
+        });
+        dayTimeout = undefined;
+      }, diffMs);
+    }
+  }
+}
+
+function checkMatchToday(client: Discord.Client, data: ESEAData) {
+  const now = moment();
+  const hasMatchToday = data.data.find((match) => {
+    const date = moment(match.date);
+    const diff = date.diff(now, "hours");
+
+    return diff > 0 && diff < 6;
+  });
+
+  if (hasMatchToday) {
+    log(`Match for today found, scheduling notification`);
+
+    const msToWarmup = moment(hasMatchToday.date)
+      .subtract(60, "minutes")
+      .diff(moment(), "ms");
+
+    // Send the warmup message 1:15 before the start date
+    if (msToWarmup - FIFTEEN_MINS > 0 && warmupTimeout === undefined) {
+      warmupTimeout = setTimeout(() => {
+        sendMessage(client, {
+          message: "WARMUP IN 15 MINUTES, GET IN HERE",
+          server: SERVER_NAME,
+          channel: CHANNEL_NAME,
+          role: ROLE_NAME,
+        });
+        warmupTimeout = undefined;
+      }, msToWarmup - FIFTEEN_MINS);
+    }
+
+    // Scold the late team members once warmup starts
+    if (msToWarmup > 0 && lateTimeout === undefined) {
+      lateTimeout = setTimeout(() => {
+        notifyWarmupLatecomers(client, SERVER_NAME, CHANNEL_NAME, lateUsers);
+        lateTimeout = undefined;
+        lateUsers = [];
+      }, msToWarmup);
+    }
+  }
+}
 
 function tick(client: Discord.Client) {
   return async () => {
@@ -36,62 +117,8 @@ function tick(client: Discord.Client) {
       }
     }
 
-    const now = moment();
-    const hasMatchTomorrow = data.data.find((match) => {
-      const date = moment(match.date);
-      const diff = date.diff(now, "hours");
-
-      return diff < 31 && diff > 24;
-    });
-
-    const hasMatchToday = data.data.find((match) => {
-      const date = moment(match.date);
-      const diff = date.diff(now, "hours");
-
-      return diff > 0 && diff < 6;
-    });
-
-    if (hasMatchTomorrow) {
-      log(`Match for tomorrow found, scheduling notification`);
-
-      // Send the message 24 hours before the start date
-      const diffMs = moment(hasMatchTomorrow.date)
-        .subtract(24, "hours")
-        .diff(moment(), "ms");
-
-      if (diffMs > 0 && dayTimeout === undefined) {
-        dayTimeout = setTimeout(() => {
-          sendEmbed(client, {
-            message: getEmbed(hasMatchTomorrow),
-            server: SERVER_NAME,
-            channel: CHANNEL_NAME,
-            role: ROLE_NAME,
-          });
-          dayTimeout = undefined;
-        }, diffMs);
-      }
-    }
-
-    if (hasMatchToday) {
-      log(`Match for today found, scheduling notification`);
-
-      // Send the warmup message 1:15 before the start date
-      const diffMs = moment(hasMatchToday.date)
-        .subtract(75, "minutes")
-        .diff(moment(), "ms");
-
-      if (diffMs > 0 && warmupTimeout === undefined) {
-        warmupTimeout = setTimeout(() => {
-          sendMessage(client, {
-            message: "WARMUP IN 15 MINUTES, GET IN HERE",
-            server: SERVER_NAME,
-            channel: CHANNEL_NAME,
-            role: ROLE_NAME,
-          });
-          warmupTimeout = undefined;
-        }, diffMs);
-      }
-    }
+    checkMatchToday(client, data);
+    checkMatchTomorrow(client, data);
     log("Finished tick");
   };
 }
@@ -99,6 +126,25 @@ function tick(client: Discord.Client) {
 async function main() {
   log("Logging into discord");
   const client = await initDiscord();
+
+  client.on("message", (message) => {
+    if (message.content.startsWith("!esea")) {
+      const command = message.content.split(" ")[1];
+      if (command === undefined) {
+        message.channel.send("Provide a command.");
+        return;
+      }
+
+      switch (command) {
+        case "late":
+          lateUsers.push(message.author.id);
+          message.channel.send(
+            `<@${message.author.id}> Added you to the late list`
+          );
+          break;
+      }
+    }
+  });
 
   tick(client)();
 
